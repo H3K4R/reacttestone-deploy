34fe4ece9203eb1dd84ea0e27d29b0d2d2196553
Swarnava Basu <basuswarnava2@gmail.com>
2020-08-02 10:56:55 +0530

    done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         My Awesome react app
+         Swarnava's Awesome react app
         </p>
         
       </header>
